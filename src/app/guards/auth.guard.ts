7fa52aefd47dc35f57faa564338d691f8d76aea5
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-27 11:55:48 +0200

    feat logging guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -13,15 +13,15 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       return new Promise( async (resolve,reject)=>{
         const user = await this.authService.getUserProfile();
-        console.log(user);
+        //console.log(user);
         if (user){
-          console.log('guard true');
+          //console.log('guard true');
           resolve(true);
           //return true;
         }else{
-          console.log('guard false');
+          //console.log('guard false');
           resolve(this.router.createUrlTree(['/login']));
-          return false;
+          //return false;
         }
       });
   }
