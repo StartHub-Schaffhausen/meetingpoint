babb8bc20caf309f66ee208da27d31aeeed455b6
Sandro Scalco <sandro.scalco@liitu.ch>
2021-06-02 11:45:40 +0200

    feat reset

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
+import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import { UserCredential } from 'src/app/models/user';
 
 @Component({
   selector: 'app-reset-password',
@@ -9,19 +11,65 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./reset-password.page.scss'],
 })
 export class ResetPasswordPage implements OnInit {
-
+  public user: UserCredential;
+  public authForm: FormGroup;
   constructor(
     private authService: AuthService,
     private alertCtrl: AlertController,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private loadingCtrl: LoadingController,
 
-  ) { }
+  ) {
+    this.authForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [''],
+    });
+   }
 
   ngOnInit() {
+    this.user = {
+      email: '',
+      password: ''
+    };
   }
 
-  resetPassword(email: string): void {
-    this.authService.resetPassword(email).then(
+  submitCredentials(authForm: FormGroup): void {
+    if (!authForm.get('email').valid) {
+      //console.log('Form is not valid yet, current value:', authForm.value);
+      this.alertCtrl.create({
+        message: 'Formular ist noch fehlerhaft',
+        buttons: [{ text: 'Ok', role: 'cancel' }],
+      }).then(alert=>{
+        alert.present();
+      });
+
+
+    } else {
+      this.presentLoading();
+      const credentials: UserCredential = {
+        email: authForm.value.email,
+        password: authForm.value.password,
+      };
+
+      this.resetPassword(credentials);
+    }
+  }
+
+  async presentLoading() {
+    const loading = await this.loadingCtrl.create({
+      cssClass: 'my-custom-class',
+      message: 'Bitte warten...',
+      duration: 2000,
+    });
+    await loading.present();
+
+    const {role, data} = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
+  }
+
+  resetPassword(credentials: UserCredential): void {
+    this.authService.resetPassword(credentials.email).then(
       async () => {
         const alert = await this.alertCtrl.create({
           message: 'Check your email for a password reset link',
