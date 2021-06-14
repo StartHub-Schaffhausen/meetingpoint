ec98afc192953b06813335d6951ddd8829b4a512
Sandro Scalco <sandro.scalco@liitu.ch>
2021-06-14 18:04:20 +0200

    feat prod

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { defineCustomElements as deckDeckGoElementQR } from '@deckdeckgo/qrcode/dist/loader';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment.prod';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
