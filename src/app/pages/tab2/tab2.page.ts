9f87c65083c8ede2242951820cf331f388767df6
Sandro Scalco <sandro.scalco@liitu.ch>
2021-10-20 10:52:54 +0200

    feat update ngdestroy

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -33,6 +33,11 @@ export class Tab2Page implements OnInit {
     private authService: AuthService
     ) {
   }
+  ngOnDestroy(){
+    //https://blog.bitsrc.io/6-ways-to-unsubscribe-from-observables-in-angular-ab912819a78f
+    //The async pipe subscribes to an Observable or Promise and returns the latest value it has emitted. When a new value is emitted, the async pipe marks the component to be checked for changes. When the component gets destroyed, the asyncpipe unsubscribes automatically to avoid potential memory leaks.
+  }
+
   async ngOnInit(){
     const user: firebase.User = await this.authService.getUser();
     if (user){
