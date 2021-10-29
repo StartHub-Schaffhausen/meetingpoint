da482e7f46b0f22040bb70259d9ae248cfa74230
Sandro Scalco <sandro.scalco@liitu.ch>
2021-10-29 18:19:20 +0200

    feat loading

--- src/app/pages/oidc/oidc.page.ts
+++ src/app/pages/oidc/oidc.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 import { first } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 import { OidcService } from 'src/app/services/oidc.service';
@@ -11,28 +12,30 @@ import { OidcService } from 'src/app/services/oidc.service';
 })
 export class OidcPage implements OnInit {
 
-  
+  loading: any;
   constructor(private route: ActivatedRoute,
     private oidc: OidcService,
     private router: Router, 
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.presentLoading();
 
     this.route.queryParams
       .subscribe(params => {
-        console.log(params);
+        /* console.log(params);
         
         console.log("code: " + params.code);
         console.log("state: " + params.state);
-
+        */ 
         /* this.oidc.getEidUserData(params.code, params.state).subscribe(data=>{
           console.log(data);
         }); */
 
         try{
           this.oidc.getEidLogin(params.code, params.state).pipe(first()).toPromise().then(data =>{
-            console.log(data);
+            //console.log(data);
             this.authService.loginWithToken(data as string).then( ()=>{
               
               this.router.navigateByUrl('/');
@@ -50,7 +53,19 @@ export class OidcPage implements OnInit {
 
   }
   ngOnDestroy(){
+    this.loading.dismiss();
+  }
 
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Please wait...',
+      //duration: 20000
+    });
+    await this.loading.present();
+
+    const { role, data } = await this.loading.onDidDismiss();
+    console.log('Loading dismissed!');
   }
 
 }
